a718d7c19886a8f56d0987af5b40f3430684ff79
Damien Whitten <dw@chaos404.com>
2017-10-24 18:52:13 -0400

    Place and Move

---
+++ robot.go
@@ -0,0 +1,125 @@
+package robot
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Robot struct {
+	Position  []int64
+	Direction *Direction
+	Dimension []Direction
+	Max       []int64
+}
+
+type Direction struct {
+	Name    string
+	Turns   map[string]*Direction
+	Forward []int64
+}
+
+const (
+	North string = "NORTH"
+	South string = "SOUTH"
+	East  string = "EAST"
+	West  string = "WEST"
+
+	Left  string = "LEFT"
+	Right string = "RIGHT"
+)
+
+// Allows configuration of custom dimensions
+var DirectionSet2D = []Direction{
+	{
+		Name:    North,
+		Forward: []int64{0, 1},
+	},
+	{
+		Name:    East,
+		Forward: []int64{1, 0},
+	},
+	{
+		Name:    South,
+		Forward: []int64{0, -1},
+	},
+	{
+		Name:    West,
+		Forward: []int64{-1, 0},
+	},
+}
+
+func init() {
+	// Implement Left and Right for 2D, easy as it is a circle.
+	for i := 0; i < 4; i++ {
+		DirectionSet2D[i].Turns = map[string]*Direction{
+			Left:  &DirectionSet2D[(i+4)%3],
+			Right: &DirectionSet2D[(i+1)%3],
+		}
+	}
+}
+
+func invalidCommand(tpl string, d ...interface{}) error {
+	return fmt.Errorf(tpl, d...)
+}
+
+var ErrorWouldFall = fmt.Errorf("Robot would fall")
+var ErrorNotPlaced = fmt.Errorf("Robot not placed")
+
+// Report returns a textual representation of the current position and direction
+func (r Robot) Report() string {
+	components := make([]string, len(r.Position), len(r.Position))
+	for idx, pos := range r.Position {
+		components[idx] = strconv.FormatInt(int64(pos), 10)
+	}
+	return fmt.Sprintf("%s,%s", strings.Join(components, ","), r.Direction.Name)
+}
+
+// Place sets the position of the robot. It must contain the same number of
+// coordinates as the board (2 for 2D)
+func (r *Robot) Place(directionName string, coordinates ...int64) error {
+	if len(coordinates) != len(r.Max) {
+		return invalidCommand("Can only move in one dimension. Please provide %d coordinate values", len(r.Position))
+	}
+
+	for idx := range coordinates {
+		if coordinates[idx] > r.Max[idx] {
+			return ErrorWouldFall
+		}
+	}
+
+	var newDirection *Direction
+	for _, direction := range r.Dimension {
+		if direction.Name == directionName {
+			newDirection = &direction
+			break
+		}
+	}
+	if newDirection == nil {
+		return invalidCommand("Invalid direction '%s' for this dimension", directionName)
+	}
+
+	r.Position = coordinates
+	r.Direction = newDirection
+
+	return nil
+}
+
+// Move moves 1 step in the current direction
+func (r *Robot) Move() error {
+	if len(r.Position) == 0 {
+		return ErrorNotPlaced
+	}
+
+	newPosition := r.Position
+	for idx := range r.Max {
+		moveTo := r.Position[idx] + r.Direction.Forward[idx]
+		if moveTo < 0 || moveTo > r.Max[idx] {
+			return ErrorWouldFall
+		}
+		newPosition[idx] = moveTo
+	}
+
+	r.Position = newPosition
+	return nil
+}
---
+++ robot_test.go
@@ -0,0 +1,124 @@
+package robot
+
+import (
+	"fmt"
+	"testing"
+)
+
+func (r Robot) assetPosition(direction string, coords ...int64) error {
+	if r.Direction == nil {
+		return fmt.Errorf("Is not placed")
+	}
+
+	if r.Direction.Name != direction {
+		return fmt.Errorf("Expected direction %s, got %s", direction, r.Direction.Name)
+	}
+
+	if len(coords) != len(r.Position) {
+		return fmt.Errorf("Robot is in the wrong dimension. Expected %v, got %v", coords, r.Position)
+	}
+
+	for idx, expect := range coords {
+		if r.Position[idx] != expect {
+
+			return fmt.Errorf("Robot is in the wrong position. Expected %v, got %v", coords, r.Position)
+		}
+	}
+
+	return nil
+}
+
+func TestPlace(t *testing.T) {
+	r := Robot{
+		Max:       []int64{1, 1},
+		Dimension: DirectionSet2D,
+	}
+
+	// Wrong number of coordinates
+	if err := r.Place(South); err == nil {
+		t.Fatal("Expected error")
+	}
+	if err := r.Place(South, 0); err == nil {
+		t.Fatal("Expected error")
+	}
+	if err := r.Place(South, 0, 0, 0); err == nil {
+		t.Fatal("Expected error")
+	}
+
+	// Made up direction
+	if err := r.Place("UP", 0, 0); err == nil {
+		t.Fatal("Expected error")
+	}
+
+	// Fall
+	if err := r.Place(South, 2, 0); err == nil {
+		t.Fatal("Expected error")
+	}
+	if err := r.Place(South, 0, 2); err == nil {
+		t.Fatal("Expected error")
+	}
+
+	if err := r.Place(North, 0, 0); err != nil {
+		t.Fatalf("Unexpected Error: %s", err.Error())
+	}
+
+	reported := r.Report()
+	if reported != "0,0,NORTH" {
+		t.Errorf("Bad Report: %s", reported)
+	}
+
+	if r.Direction.Name != North {
+		t.Errorf("Expected North, got %s", r.Direction.Name)
+	}
+
+	if len(r.Position) != 2 ||
+		r.Position[0] != 0 ||
+		r.Position[1] != 0 {
+		t.Errorf("Wrong Position: %v", r.Position)
+	}
+}
+
+func TestMove(t *testing.T) {
+	r := Robot{
+		Max:       []int64{2, 2},
+		Dimension: DirectionSet2D,
+	}
+
+	if err := r.Move(); err == nil {
+		t.Error("Expected error")
+	} else if err != ErrorNotPlaced {
+		t.Errorf("Wrong error: %s\n", err.Error())
+	}
+
+	// On a 3x3 grid, Move one step from the center in each direction, check
+	// position, then try to fall
+
+	for direction, position := range map[string][]int64{
+		North: {1, 2},
+		South: {1, 0},
+		East:  {2, 1},
+		West:  {0, 1},
+	} {
+
+		if err := r.Place(direction, 1, 1); err != nil {
+			t.Fatalf("Unexpected Error: %s", err.Error())
+		}
+		if err := r.assetPosition(direction, 1, 1); err != nil {
+			t.Fatalf("Testing %s: %s", direction, err.Error())
+		}
+		if err := r.Move(); err != nil {
+			t.Fatalf("Testing %s: %s", direction, err.Error())
+		}
+		if err := r.assetPosition(direction, position...); err != nil {
+			t.Fatalf("Testing %s: %s", direction, err.Error())
+		}
+
+		// Try to move once more, would fall
+		if err := r.Move(); err == nil {
+			t.Error("Expected error")
+		} else if err != ErrorWouldFall {
+			t.Errorf("Wrong error: %s\n", err.Error())
+		}
+	}
+
+}
