4376c06588b11ff9b1107b48f65ea0d92edbc600
Damien Whitten <dw@chaos404.com>
2017-10-25 09:24:44 -0400

    Config flags

--- cmd/robot/robot.go
+++ cmd/robot/robot.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"flag"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 
-	robot "github.com/daemonl/go_robot"
+	"github.com/daemonl/go_robot"
 )
 
+var maxX int64
+var maxY int64
+var quiet bool
+
+func init() {
+	flag.Int64Var(&maxX, "max-x", 4, "Maximum board X value")
+	flag.Int64Var(&maxY, "max-y", 4, "Maximum board Y value")
+	flag.BoolVar(&quiet, "quiet", false, "Disable error output (silently ignores bad commands)")
+}
+
 func main() {
+	flag.Parse()
 	r := &robot.Robot{
 		Dimension: robot.DirectionSet2D,
-		Max:       []int64{5, 5},
+		Max:       []int64{maxX, maxY},
 	}
 
-	if err := robot.CommandStream(r, os.Stdin, os.Stdout, os.Stderr); err != nil {
+	var errorOutput io.Writer = os.Stdout
+	if quiet {
+		errorOutput = ioutil.Discard
+	}
+	if err := robot.CommandStream(r, os.Stdin, os.Stdout, errorOutput); err != nil {
 		log.Fatal(err.Error())
 	}
 }
